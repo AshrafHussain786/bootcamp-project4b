ba08d8210a557fd4125c6cc38796747341daa226
Muhammad Ashraf <ashrafhussain786@yahoo.com>
2020-08-04 00:41:34 +0500

    Updated App

--- src/components/Sdata.js
+++ src/components/Sdata.js
@@ -1,33 +1,34 @@
-import web1 from "../images/freeimg1.jpg";
-import web2 from "../images/freeimg2.jpg";
-import web3 from "../images/freeimg3.jpg";
-import web4 from "../images/bootcamp.jpg";
+// import web1 from "../images/freeimg1.jpg";
+// import web2 from "../images/freeimg2.jpg";
+// import web3 from "../images/freeimg3.jpg";
+// import web4 from "../images/Plateform.jpg";
+import web from "../images/bootcamp.jpg";
 
 const Sdata = [
     {
         id: 1,
-        imgsrc: web1,
+        imgsrc: web,
         title: "Project 1: Expense Tracker",
         surge: "http://bootcamp-tracker-ashraf.surge.sh",
         repo: "https://github.com/AshrafHussain786/Expense-Tracker-React",
     },
     {
         id: 2,
-        imgsrc: web2,
+        imgsrc: web,
         title: "Project 2: Covid-19 App",
         surge: "http://bootcamp-covid19react-ashraf.surge.sh",
         repo: "https://github.com/AshrafHussain786/covid19-react",
     },
     {
         id: 3,
-        imgsrc: web3,
+        imgsrc: web,
         title: "Project 3: Shoe Store",
         surge: "http://bootcamp-shoe-store-ashraf.surge.sh/",
         repo: "https://github.com/AshrafHussain786/Bootcamp-Shoe-Store",
     },
     {
         id: 4,
-        imgsrc: web4,
+        imgsrc: web,
         title: "Project 4A: Red Queen's Race",
         surge: "http://bootcamp-project4a-ashraf.surge.sh/",
         repo: "https://github.com/AshrafHussain786/bootcamp2020-project4A",
